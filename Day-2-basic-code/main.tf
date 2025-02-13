ed15f6d062f2fd963ff6e0bdffb9df8bb8c7cc25
veera <veerababu.narni232@gmail.com>
2025-02-13 10:05:25 +0530

    day-1,2

--- Day-2-basic-code/main.tf
+++ Day-2-basic-code/main.tf
@@ -1,1 +1,6 @@
-
+resource "aws_instance" "name" {
+    ami = var.ami_id
+    instance_type = var.type
+    key_name = var.key
+  
+}
---
+++ Day-2-basic-code/variables.tf
@@ -0,0 +1,18 @@
+variable "ami_id" {
+    description = "inserting ami value"
+    type = string
+    default = ""
+  
+}
+
+variable "type" {
+    type = string
+    default = "t2.nano"
+  
+}
+
+variable "key" {
+    type = string
+    default = ""
+  
+}
