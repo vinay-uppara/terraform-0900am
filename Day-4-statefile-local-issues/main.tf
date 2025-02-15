32e316393c76f9cbe957df1de7423de582a55b90
veera <veerababu.narni232@gmail.com>
2025-02-15 09:55:24 +0530

    Day4 remote

--- Day-4-statefile-local-issues/main.tf
+++ Day-4-statefile-local-issues/main.tf
@@ -9,3 +9,10 @@ resource "aws_instance" "name" {
    
   
 }
+
+
+#note Developers Overwriting Each Other’s Work if separe state file for same project
+#Merge Conflicts in terraform.tfstate
+#If two developers run terraform apply at the same time, one may overwrite the other's changes, causing unexpected resource modifications.
+#both developers working independetly not collabrative 
+#solution: maintain common ststefile to overocme above issues 
