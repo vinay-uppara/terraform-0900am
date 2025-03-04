b2e7647698adef8dc5acd7c1a12d96862c4dbd32
vinay-uppara <vinaykumaruppara36@gmail.com>
2025-03-04 14:57:56 +0530

    Update variables.tf

--- Day-2-basic-code-for-module-source/variables.tf
+++ Day-2-basic-code-for-module-source/variables.tf
@@ -1,18 +1,18 @@
 variable "ami_id" {
     description = "inserting ami value"
     type = string
-    default = ""
+    default = "ami-05b10e08d247fb927"
   
 }
 
 variable "type" {
     type = string
-    default = ""
+    default = "t2.micro"
   
 }
 
 variable "key" {
     type = string
-    default = ""
+    default = "vin"
   
 }
